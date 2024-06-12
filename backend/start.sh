435a77db363711ddb212ae005ae29e1bac1b9314
blockchainlone <167520551+blockchainlone@users.noreply.github.com>
2024-06-12 02:23:45 -0400

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ensure the script uses bash version 4.0 or later
+if [ "${BASH_VERSINFO:-0}" -lt 4 ]; then
+  echo "This script requires bash version 4.0 or later."
+  exit 1
+fi
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
 
@@ -7,29 +13,29 @@ KEY_FILE=.webui_secret_key
 
 PORT="${PORT:-8080}"
 HOST="${HOST:-0.0.0.0}"
-if test "$WEBUI_SECRET_KEY $WEBUI_JWT_SECRET_KEY" = " "; then
+
+if [ -z "$WEBUI_SECRET_KEY" ] && [ -z "$WEBUI_JWT_SECRET_KEY" ]; then
   echo "Loading WEBUI_SECRET_KEY from file, not provided as an environment variable."
 
-  if ! [ -e "$KEY_FILE" ]; then
+  if [ ! -e "$KEY_FILE" ]; then
     echo "Generating WEBUI_SECRET_KEY"
     # Generate a random value to use as a WEBUI_SECRET_KEY in case the user didn't provide one.
-    echo $(head -c 12 /dev/random | base64) > "$KEY_FILE"
+    head -c 12 /dev/random | base64 > "$KEY_FILE"
   fi
 
   echo "Loading WEBUI_SECRET_KEY from $KEY_FILE"
   WEBUI_SECRET_KEY=$(cat "$KEY_FILE")
 fi
 
-if [[ "${USE_OLLAMA_DOCKER,,}" == "true" ]]; then
+if [ "${USE_OLLAMA_DOCKER,,}" == "true" ]; then
     echo "USE_OLLAMA is set to true, starting ollama serve."
     ollama serve &
 fi
 
-if [[ "${USE_CUDA_DOCKER,,}" == "true" ]]; then
+if [ "${USE_CUDA_DOCKER,,}" == "true" ]; then
   echo "CUDA is enabled, appending LD_LIBRARY_PATH to include torch/cudnn & cublas libraries."
   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib/python3.11/site-packages/torch/lib:/usr/local/lib/python3.11/site-packages/nvidia/cudnn/lib"
 fi
-
 
 # Check if SPACE_ID is set, if so, configure for space
 if [ -n "$SPACE_ID" ]; then
@@ -43,11 +49,10 @@ if [ -n "$SPACE_ID" ]; then
       sleep 1
     done
     echo "Creating admin user..."
-    curl \
-      -X POST "http://localhost:8080/api/v1/auths/signup" \
-      -H "accept: application/json" \
-      -H "Content-Type: application/json" \
-      -d "{ \"email\": \"${ADMIN_USER_EMAIL}\", \"password\": \"${ADMIN_USER_PASSWORD}\", \"name\": \"Admin\" }"
+    curl -X POST "http://localhost:8080/api/v1/auths/signup" \
+         -H "accept: application/json" \
+         -H "Content-Type: application/json" \
+         -d "{ \"email\": \"${ADMIN_USER_EMAIL}\", \"password\": \"${ADMIN_USER_PASSWORD}\", \"name\": \"Admin\" }"
     echo "Shutting down webui..."
     kill $webui_pid
   fi
